c68758e3ff6f6d80f8d0990f716a38f5ad61332a
chesiredk <100325585+chesiredk@users.noreply.github.com>
2022-02-23 20:56:03 -0500

    Add files via upload
    OleArcarde is written in python with turtle library. Its  basically structured as a maze that represents a forest. And you are supposed to navigate the Derrick, to the lion in the maze, upon reaching proximity to the lion; a reward is given.

---
+++ Olearcade.py
@@ -0,0 +1,230 @@
+import turtle
+import random as rand
+
+turtle.title("Derrick Moran")
+
+# Define some functions create the jungle using horizontal and vertical lists.
+pixels = list()
+
+
+# horizontal forest line
+def mark_horizontal_line(lo, x, y):
+    line_horizontal = turtle.Turtle()
+    line_horizontal.speed(0)
+    line_horizontal.hideturtle()
+    line_horizontal.up()
+    line_horizontal.setposition(x, y)
+    for n in lo:
+        if n == 1:
+            for i in range(2):
+                line_horizontal.forward(21 / 4)
+                pixel = line_horizontal.clone()
+                pixel.showturtle()
+                pixel.shape("square")
+                pixel.shapesize(1 / 2, 10.5 / 20)
+                pixel.color("green")
+                pixels.append(pixel)
+                line_horizontal.forward(21 / 4)
+
+        if n == 0:
+            line_horizontal.forward(21)
+
+
+def mark_horizontal_lines(lo, x, y):
+    step_y = y
+    for n in lo:
+        mark_horizontal_line(n, x, step_y)
+        step_y -= 21
+
+
+# vertical forest line
+def mark_line_vertical(lv, x, y):
+    line_v = turtle.Turtle()
+    line_v.hideturtle()
+    line_v.speed(0)
+    line_v.up()
+    line_v.setposition(x, y)
+    line_v.right(90)
+    for n in lv:
+        if n == 1:
+            for i in range(2):
+                line_v.forward(21 / 4)
+                pixel = line_v.clone()
+                pixel.showturtle()
+                pixel.shape("circle")
+                pixel.shapesize(1 / 2, 10.5 / 20)
+                pixel.color("green")
+                pixels.append(pixel)
+                line_v.forward(21 / 4)
+        if n == 0:
+            line_v.forward(21)
+
+
+def create_lines_vertical(lv, x, y):
+    step_dx = x
+    for n in lv:
+        mark_line_vertical(n, step_dx, y)
+        step_dx += 21
+
+
+# this function connects the entire forest(maze like) joining the horizontal and vertical lines
+
+def mark_park(lo, lv, x, y):
+    mark_horizontal_lines(lo, x, y)
+    create_lines_vertical(lv, x, y)
+
+
+# display window
+olemoran = turtle.Screen()
+olemoran.screensize(400, 400)
+olemoran.title("Derrick Moran")
+olemoran.bgcolor("beige")
+
+# The actual list
+lo = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+lv = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# i call the function
+turtle.tracer(0)
+mark_park(lo, lv, -200, 300)
+turtle.update()
+turtle.tracer(1)
+
+# creating Lion as Mufasa
+simba_position = (rand.randint(-150, 150), rand.randint(-100, 150))  # its position in the park
+simba = turtle.Turtle()
+olemoran.addshape("ezgif.com-gif-maker.gif")
+simba.speed(0)
+simba.shape("ezgif.com-gif-maker.gif")
+simba.up()
+simba.setposition(simba_position)
+simba.up()
+simba.down()
+simba.penup()
+
+# creating Derrick as Ole
+starting_pos = (63, -162)  # Derrick's entry point to the forest
+ole = turtle.Turtle()
+olemoran.addshape("ezgif.com-gif-maker (1).gif")
+ole.speed(0)
+ole.shape("ezgif.com-gif-maker (1).gif")
+ole.up()
+ole.setposition(starting_pos)
+ole.color("blue")
+ole.up()
+ole.down()
+ole.penup()
+
+
+def forward(turt):
+    for i in range(11):
+        turt.forward(1)
+        if any(pixel.distance(turt) < 5 for pixel in pixels):
+            for i in range(i):
+                turt.undo()
+            break
+
+
+def dx():
+    ole.speed(5)
+    forward(ole)
+    ole.speed(0)
+
+
+def sx():
+    ole.right(180)
+    ole.speed(5)
+    forward(ole)
+    ole.speed(0)
+    ole.right(-180)
+
+
+def down():
+    ole.right(90)
+    ole.speed(5)
+    forward(ole)
+    ole.speed(0)
+    ole.right(-90)
+
+
+def up():
+    ole.right(-90)
+    ole.speed(5)
+    forward(ole)
+    ole.speed(0)
+    ole.right(90)
+
+
+def close():
+    olemoran.bye()
+
+
+olemoran.listen()
+olemoran.onkey(dx, "Right")
+olemoran.onkey(sx, "Left")
+olemoran.onkey(up, "Up")
+olemoran.onkey(down, "Down")
+olemoran.onkey(close, "q")
+
+# what to happen upon lion capture
+
+while True:
+    olemoran.update()
+    if ole.distance(simba) <= 20:
+        reward = turtle.Turtle()
+        olemoran.addshape("ezgif.com-gif-maker-trophy.gif")
+        reward.shape("ezgif.com-gif-maker-trophy.gif")
+        reward.speed(0)
+        reward.color("red")
+        reward.up()
+        reward.setposition(simba_position)
+        reward.down()
+        reward.pendown()
